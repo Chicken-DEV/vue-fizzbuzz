f76cc59fc189a428c94ed8b87ad23e2bd2ecc95c
Udomomo <batzuma@yahoo.co.jp>
2018-06-03 21:07:05 +0900

    コンポーネントを追加

---
+++ src/components/Buttons.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="buttons">
+      <div class="button">
+          <div class="thumbnail">
+            <img src="/static/images/none.png" alt="none">
+          </div>
+          <div>
+            <p>Not divisible</p>
+          </div>
+      </div>
+      <div class="button">
+          <div class="thumbnail">
+            <img src="/static/images/fizz.png" alt="fizz">
+          </div>
+          <div>
+            <p>Fizz</p>
+          </div>
+      </div>
+      <div class="button">
+          <div class="thumbnail">
+            <img src="/static/images/buzz.png" alt="buzz">
+          </div>
+          <div>
+            <p>Buzz</p>
+          </div>
+      </div>
+      <div class="button">
+          <div class="thumbnail">
+            <img src="/static/images/fizzbuzz.png" alt="fizzbuzz">
+          </div>
+          <div>
+            <p>FizzBuzz</p>
+          </div>
+      </div>
+  </div>
+</template>
+
+<style scoped>
+
+.buttons {
+  margin: 0 auto;
+  width: 50%;
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+}
+
+.buttons .button {
+  display: inline-block;
+  margin: 1%;
+  width: 46%;
+  border: solid;
+  border-color: #83cd29;
+  border-radius: 10px;
+  text-align: center;
+}
+
+.thumbnail > img {
+  margin-top: 20px;
+  width: 40%;
+}
+</style>
+
+
---
+++ src/components/Fizzbuzz.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="fizzbuzz">
+      <div id="currentScore">
+        <p>Score: 0</p>
+      </div>
+      <div id="question">
+        <p id="nextNumber">13</p>
+      </div>
+      <div id="highScore">
+        <p>High score: 0</p>
+      </div>
+    </div>
+</template>
+
+<style scoped>
+
+.fizzbuzz{
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  width: 50%;
+}
+
+.fizzbuzz > div {
+  margin: 10px;
+  width: 33%;
+  display: flex;
+  align-items: center;
+}
+
+#currentScore {
+  justify-content: flex-start;
+}
+
+#highScore {
+  justify-content: flex-end;
+}
+
+#question {
+  justify-content: center;
+}
+
+#nextNumber {
+  font-size: 250%;
+  margin: 0px;
+}
+</style>
