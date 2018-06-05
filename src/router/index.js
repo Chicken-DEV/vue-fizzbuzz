43eac15b4d271b32462bcf81cb884b515533d1e6
Udomomo <batzuma@yahoo.co.jp>
2018-06-05 23:21:52 +0900

    export defaultのコメントアウトを外した

--- src/router/index.js
+++ src/router/index.js
@@ -2,14 +2,13 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 Vue.use(Router)
-/*
+
 export default new Router({
-  routes: [
+  /* routes: [
     {
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
     }
-  ]
+  ] */
 })
-*/
